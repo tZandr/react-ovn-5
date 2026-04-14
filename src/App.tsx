551f94e97243e1687f60aba3d65ef26a72c596b9
tZandr <alex_tjernstrom@hotmail.com>
2026-04-14 22:03:41 +0200

    cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
+import { Card } from './components/Card';
 
 function App() {
-
   return (
     <>
       <div id="root">
@@ -10,6 +10,18 @@ function App() {
           <p>x</p>
         </div>
         <div className="assignment">
+          <Card title="User">
+            <p>Name: John Doe</p>
+            <p>Email: john.doe@example.com</p>
+          </Card>
+          <Card title="Favorite Game">
+            <p>Name: Pokémon Colosseum</p>
+            <p>Publisher: Nintendo</p>
+          </Card>
+          <Card title="Favorite Film">
+            <p>Inception</p>
+            <p>Producer: Christopher Nolan</p>
+          </Card>
         </div>
         <div>
           <small>Alexander Tjernström, IT-Högskolan JSU25</small>
